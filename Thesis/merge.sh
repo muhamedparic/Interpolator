2edbef4bb50c545c49e10eb9b18b37973a593824
muhamedparic <mparic1@etf.unsa.ba>
2017-09-20 01:19:51 +0200

    Finalna verzija

--- Thesis/merge.sh
+++ Thesis/merge.sh
@@ -1,1 +1,1 @@
-pdftk "chapters/Naslovna strana.pdf" "Tehnike interpolacije frejmova u video sekvencama.pdf" cat output "Finalna verzija/Tehnike interpolacije frejmova u video sekvencama.pdf"
+pdftk "chapters/Naslovna strana.pdf" "chapters/Postavka.pdf" "Tehnike interpolacije frejmova u video sekvencama.pdf" cat output "Finalna verzija/Tehnike interpolacije frejmova u video sekvencama.pdf"
